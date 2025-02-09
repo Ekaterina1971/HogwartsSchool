18d0bb8fec7b2076144bc17e5140f6ae2c9b3687
Ekaterina1971 <ivanova71dom@yandex.ru>
2025-02-09 21:35:58 +0300

    продолжение работы 4

--- src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
+++ src/main/java/ru/hogwarts/school/repository/AvatarRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface AvatarRepository extends JpaRepository<Avatar, Long> {
-    public Optional<Avatar> findByStudentId(Long studentId);
+     Optional<Avatar> findByStudentId(Long studentId);
 }
--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -1,24 +1,67 @@
 package ru.hogwarts.school.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
+import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
-import ru.hogwarts.school.repository.StudentRepository;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 @Transactional
 public class AvatarService {
-    @Value("${avatars.dir.path}")
+    @Value("${path.to.avatars.folder}")
     private String avatarsDir;
 
-    private final StudentRepository studentRepository;
     private final AvatarRepository avatarRepository;
+    private final StudentService studentService;
 
-    public AvatarService(StudentRepository studentRepository, AvatarRepository avatarRepository) {
+    public AvatarService(AvatarRepository avatarRepository, StudentService studentService) {
         this.avatarRepository = avatarRepository;
-        this.studentRepository = studentRepository;
-
+        this.studentService = studentService;
     }
+    public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        Student student = studentService.findStudent(studentId);
+//        хранит путь до директории с загружаемыми файлами.
+        Path filePath = Path.of(avatarsDir, student + "." + getExtensions(avatarFile.getOriginalFilename()));
+//        Создаем нужную нам директорию для хранения данных и удаляем из нее файл, если он уже присутствует там.
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+//        конструкция нам нужна, чтобы следить за закрытием открытых ресурсов
+        try (
+//                чтение файла. Открываем входной поток командой avatarFile.getInputStream() и начинаем считывать данные
+                InputStream is = avatarFile.getInputStream();
+//                запись файла
+                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+//                буферизация для чтения не по байтно, а частями заданного размера
+                BufferedInputStream bis = new BufferedInputStream(is, 1024);
+//                для записи
+                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+        ) {
+//            запустить сам процесс передачи данных методом transferTo.
+            bis.transferTo(bos);
+        }
+        Avatar avatar = new Avatar();
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(avatarFile.getSize());
+        avatar.setMediaType(avatarFile.getContentType());
+        avatar.setData(avatarFile.getBytes());
+        avatarRepository.save(avatar);
+    }
+    private String getExtensions(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+    public Avatar findAvatar(Long studentId) {
+        return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
+    }
+
 
 }
