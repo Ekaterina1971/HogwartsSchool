786ed42cd7e48f53e02bf58844ffafbf9827bb27
Ekaterina1971 <ivanova71dom@yandex.ru>
2025-03-05 23:24:52 +0300

    задание миграции и индексы

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,6 +3,8 @@ package ru.hogwarts.school.service;
 import java.util.Collection;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
@@ -10,33 +12,39 @@ import ru.hogwarts.school.repository.StudentRepository;
 
 @Service
 public class StudentService {
+    private static final Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     @Autowired
     private StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
-
         this.studentRepository = studentRepository;
     }
 
     public Student addStudent(Student student) {
-
+        logger.info("The student has been added to the application");
         return studentRepository.save(student);
     }
-    public Student findStudent(long id) {
 
+    public Student findStudent(long id) {
+        logger.info("findStudent method has been invoked");
+        logger.debug("Requesting info for student with id: {}, id");
         return studentRepository.findById(id).get();
     }
 
     public Student editStudent(Student student) {
-
+        logger.info("editStudent method has been invoked");
+        logger.error("There is no student with id");
         return studentRepository.save(student);
     }
 
     public void deleteStudent(long id) {
+        logger.info("The student of ID has been deleted");
         studentRepository.deleteById(id);
     }
 
     public Collection<Student> getAllStudent() {
+        logger.info("getAllStudent method has been invoked");
         return studentRepository.findAll();
     }
 
